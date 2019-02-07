40a72449851ab99b7a8b9c3eb2dea5819bddef59
hulkkamedise <31106981+hulkkamedise@users.noreply.github.com>
2019-02-07 19:20:09 +0000

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "talkingspace");
+
+define("SITE_TITLE", "Welcome To TalkingSpace!");
+
+//Paths
+define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/talkingspace/');
